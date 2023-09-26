a9c1bb33facdabdb882c66aa448f57aec7792059
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-26 05:47:43 -0700

    commit first

--- deleElementarray.js
+++ deleElementarray.js
@@ -8,13 +8,16 @@ for( let i=0; i<array2.length-1;i++)
 {
     console.log(array2[i])
 }
-console.log("delete element from any position")
-let positon3 = 5;
-for(let i=positon3;i<array2.length-2;i++)
+console.log("delete")
+function delet()
+{
+    let positon3 = document.getElementById("pos").value;
+    for(let i=positon3-1;i<array2.length-2;i++)
 {
     array2[i]=array2[i+1];
 }
 for(let i=0; i<array2.length-2;i++)
 {
     console.log(array2[i])
+}
 }
