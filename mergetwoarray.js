89b997bbd03ecf9ef43e1719920cdc023f9ae5fb
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-28 10:29:58 -0700

    commit first

--- mergetwoarray.js
+++ mergetwoarray.js
@@ -1,6 +1,7 @@
 let array1 = [2,4,6,8];
 let array2 = [1,3,5];
 let array3 = [...array1,...array2];
+let array4 = [];
 console.log(array3)
 console.log("sorting in acessending form")
 array3.sort((a,b)=> a-b)
@@ -8,3 +9,13 @@ console.log(array3  )
 console.log("sorting in dessending form")
 array3.sort((a,b)=> b-a)
 console.log(array3)
+console.log("merging two array in simpale way using loop")
+for(let i=0;i<array1.length;i++)
+{
+    array4[i] = array1[i];
+}
+for(let i=0;i<array2.length;i++)
+{
+    array4[array1.length+i]= array2[i];
+}
+console.log(array4)
