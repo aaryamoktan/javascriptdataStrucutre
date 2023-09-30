27f8c904bc4cf1e83f090becdccf5c48796ae6c9
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-30 11:51:22 -0700

    commit first

--- stringgg.js
+++ stringgg.js
@@ -9,3 +9,19 @@ for(let i=len-1;i>=0;i--)
     newstring +=name1[i]
 }
 console.log(newstring)
+console.log("word reverse question in leetcode")
+let lent = name1.length-1
+
+let ansstr=""
+let i=name1.length-1;
+let name3 = "hi i love you"
+let name2= name3.split(' ')
+console.log(name2.length)
+for(let i = name2.length-1;i>=0;i--)
+{
+    if(name2[i] == '') continue;
+    if(ansstr.length>0) ansstr+=' ';
+    ansstr+=name2[i]
+    
+}
+console.log(ansstr)
