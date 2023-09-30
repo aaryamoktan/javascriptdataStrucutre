3d515a57d60cbf34e7d651ec1289643c2c8cb1d6
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-30 10:57:02 -0700

    commit first

--- reserve.js
+++ reserve.js
@@ -6,3 +6,11 @@ for (let i=aaray.length;i>0;i--)
     console.log(arra2)
 }
 
+let aaray1 = ["a","b","c","d","e","f","g"];
+let arra21 = []
+for (let i=aaray1.length;i>0;i--)
+{
+    arra21= aaray1.pop()
+    console.log(arra21)
+}
+
