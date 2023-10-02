190c3082493e460f71fd14ca3eca7432015cda89
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-02 00:32:20 -0700

    commit first

--- linklists.js
+++ linklists.js
@@ -5,8 +5,25 @@ class list{
         this.head={
             value:data,
             next:null
+        };
+        this.tail = this.head;
+        this.size=1;
+    }
+    appendnode(nodedata)
+    {
+        let newnode={
+            value:nodedata,
+            next:null
         }
+        this.tail.next=newnode;
+        this.tail=newnode
+        this.size++;    
+
     }
 }
 let newlist = new list(200);
+console.log(newlist)
+newlist.appendnode(300)
+newlist.appendnode(400)
+newlist.appendnode(500)
 console.log(newlist)
