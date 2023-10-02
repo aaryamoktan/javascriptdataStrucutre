a88bf3c999b467ee8af57b50b67fa5950b6fe338
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-02 00:20:00 -0700

    commit first

--- linklists.js
+++ linklists.js
@@ -1,1 +1,12 @@
 console.log("linklist")
+class list{
+    constructor(data)
+    {
+        this.head={
+            value:data,
+            next:null
+        }
+    }
+}
+let newlist = new list(200);
+console.log(newlist)
