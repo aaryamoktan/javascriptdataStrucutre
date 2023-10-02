5dd417eb5ac43cc464408e3f97db5408df53adf4
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-02 00:14:25 -0700

    commit first

--- missingele.js
+++ missingele.js
@@ -1,13 +1,13 @@
 let array =[1,2,3,5,6];
 let sum=0;
 let n = array.length+1;
-console.log(n)
+console.log(n);
 for(let i=0;i<array.length;i++)
 {
-    sum += array[i]
+    sum += array[i];
 }
 let sum2 = n*(n+1)/2;
-console.log(sum2)
+console.log(sum2);
 let missingnumber =  sum2-sum;
 console.log(missingnumber)
 
--- set.js
+++ set.js
@@ -1,12 +1,8 @@
-
-
 let seet = new Set();
 let array = ["aarya","asmita","moktan","asmita"]
 seet.add("aarya")
-
 for(let i=0;i<array.length;i++)
 {
     seet.add(array[i])
 }
 console.log(seet)
- 
