0d468a483fb47c89459dd2e1f13e7b75f6384632
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-03 06:39:06 -0700

    commmit first

--- missingele.js
+++ missingele.js
@@ -1,13 +1,32 @@
 let array =[1,2,3,5,6];
 let sum=0;
 let n = array.length+1;
-console.log(n);
+
 for(let i=0;i<array.length;i++)
 {
     sum += array[i];
 }
 let sum2 = n*(n+1)/2;
-console.log(sum2);
-let missingnumber =  sum2-sum;
-console.log(missingnumber)
 
+let missingnumber =  sum2-sum;
+
+let arra303 = [1,4,5,6,3]
+
+let i=1;
+arra303.sort((a,b)=>a-b)
+console.log(arra303);
+let solut = [];
+let j=1;
+
+while(i<arra303.length)
+{
+    if(i==arra303[j])
+    {
+        i++;
+        j++;
+    }
+    else{
+        console.log(i)
+        i++;
+    }
+}
