979d2e0e684b0e2de24a919f111a42c4ec9701f7
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-04 02:52:57 -0700

    commit first

---
+++ linearsearch.js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5,6,7];
+let data=3;
+let i = 0;
+while(i<array.length)
+{
+    if(data == array[i])
+    {
+        console.log(i)
+    }
+    i++;
+}
--- missingele.js
+++ missingele.js
@@ -10,7 +10,7 @@ let sum2 = n*(n+1)/2;
 
 let missingnumber =  sum2-sum;
 
-let arra303 = [1,4,5,6,3]
+let arra303 = [1,2,5,6,3]
 
 let i=1;
 arra303.sort((a,b)=>a-b)
@@ -20,13 +20,16 @@ let j=1;
 
 while(i<arra303.length)
 {
-    if(i==arra303[j])
+    if(i!=arra303[j])
     {
-        i++;
-        j++;
-    }
-    else{
         console.log(i)
         i++;
+        
+        
+    }
+    else{
+        
+        i++;
+        j++;
     }
 }
