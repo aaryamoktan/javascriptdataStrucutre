e67c52f4c73f47d01dfd8e130ee2ca22c03a9a85
aarya moktan <aaryamoktan2055@gmail.com>
2024-01-05 02:12:16 -0800

    commit first

--- Arrayyy.js/BinarySearch.js
+++ Arrayyy.js/BinarySearch.js
@@ -1,22 +1,29 @@
-const arr = [1,2,3,4,5,6,7,8,9]
-const target = 3;
-var start = 0;
-var end =arr.length-1;
-while(start<=end)
-{
-  let mid = Math.floor((start+end)/2);
-  if(target== arr[mid])
-  {
-    console.log(mid);
-    break;
-  }
-  else if (target> arr[mid])
-  {
-    start = mid+1;
-  }
-  else if(target< arr[mid])
-  {
-    end = mid-1;
-  }
- 
-}
+//const arr = [1,2,3,4,5,6,7,8,9]
+//const target = 3;
+//var start = 0;
+//var end =arr.length-1;
+//while(start<=end)
+//{
+//  let mid = Math.floor((start+end)/2);
+//  if(target== arr[mid])
+//  {
+//    console.log(mid);
+//    break;
+//  }
+//  else if (target> arr[mid])
+//  {
+//    start = mid+1;
+//  }
+//  else if(target< arr[mid])
+//  {
+//    end = mid-1;
+//  }
+//}
+console.log("celling number");
+var target1 = 11;
+var arr1 = [1,3,4,7,9,11,13,14,15];
+let start1 = 0;
+let end1 = arr1.length-1;
+console.log(end1)
+let ans= 0 ;
+const arr3 =[]
