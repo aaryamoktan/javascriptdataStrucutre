cc64f9d637637c3fd0f5dc1a0c1a8d39544d74fc
aarya moktan <aaryamoktan2055@gmail.com>
2024-06-11 21:26:27 -0700

    first commit

--- secconl.js
+++ secconl.js
@@ -14,4 +14,25 @@ const finds = ((arr)=>
     })
     const nums = [10,10,10,10,10,10,2]
     console.log(finds(nums))
+   // another approach optimal
+   //const finds = ((arr)=>
+   // {
+   //    let fl = 0;
+   //    let sl = 0;
+   //    for(let i = 0; i<arr.length;i++)
+   //    {
+   //        if(arr[i]>fl)
+   //        {
+   //            sl = fl;
+   //            fl=arr[i];
+   //        }
+   //        else if(arr[i]!=fl&& arr[i]>sl)
+   //        {
+   //            sl= arr[i]
+   //        }
+   //    }
+   //    return sl
+   // })
+   // const nums = [1,2,3,44,55,44,55,66,77]
+   // console.log(finds(nums))
     
