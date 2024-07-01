fed2a888094d9cba8b7af4fa627aeb91b4f393aa
aarya moktan <aaryamoktan2055@gmail.com>
2024-07-01 07:40:06 -0700

    first commit

--- Arrayyy.js/rot.js
+++ Arrayyy.js/rot.js
@@ -9,4 +9,30 @@ function rota(arrat,a)
     arrat.unshift(...rotate)
     return arrat
 }
-console.log(rota([1,2,3,4,5,6,7,8,9],1))
+//console.log(rota([1,2,3,4,5,6,7,8,9],1))
+
+
+function rotaa(arrat,a)
+{
+   let size = arrat.length;
+   if(a>size)
+    {
+        a=a%10;
+    }
+    rotater(arrat,0,arrat.length-1)
+    rotater(arrat,0,a-1)
+    rotater(arrat,a,size-1)
+
+    return arrat;
+}
+function rotater(nums,left,right)
+{
+    while(left<right)
+        {
+    let temp = nums[left];
+    nums[left++]=nums[right];
+    nums[right--] = temp;
+        }   
+}
+
+console.log(rotaa([1,2,3,4,5,6,7,8,9],3))
